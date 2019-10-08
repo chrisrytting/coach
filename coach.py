430d832e099d68a906654db3e9f9b108b8c37071
chrisrytting <christophermichaelrytting@gmail.com>
2019-10-07 21:50:27 -0600

    added docstring

--- coach.py
+++ coach.py
@@ -1,4 +1,9 @@
 class Coach():
+    """
+    This class is a coach which allows you to query a database of 
+    old questions you've had to answer for yourself. It's a way of
+    outsourcing decision-making to a wiser you.
+    """
     def __init__(self):
         print("Hi, I'm coach!")
 
