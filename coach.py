8c93f1405d91162ad56f56eb919dba12b4e47d9f
chrisrytting <christophermichaelrytting@gmail.com>
2019-10-09 22:42:49 -0600

    added a method for asking questions. throws syntax error rn

--- coach.py
+++ coach.py
@@ -3,13 +3,29 @@ class Coach():
     This class is a coach which allows you to query a database of 
     old questions you've had to answer for yourself. It's a way of
     outsourcing decision-making to a wiser you.
+
+    The process goes as follows: I instantiate coach, and it asks me
+
+    "What do you need advice on?"
+
+    At this point, I provide a question, and the coach class runs 
+    a query on my local database of former questions asked. If I've 
+    asked the question before, then a match will be given. 
     """
     def __init__(self):
         print("Hi, I'm coach!")
+
+    def ask_question(self):
+        """
+        This method is for users to ask coach a question
+        """
+        val = input('What is your question?')
+        print(val)
 
 
 
 if __name__=="__main__":
     
     c = Coach()
+    c.ask_question()
     
